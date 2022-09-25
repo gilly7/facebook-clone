fb1864fa84fc6b85d81cd7b459860e4edf147821
Gilbert Ng'eno <ngenogilbert07@gmail.com>
2022-09-25 15:20:34 +0300

    create SidebarItem

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Image from "next/image";
+import SidebarItem from "./SidebarItem";
 import { ImUsers } from "react-icons/im";
+import { MdGroups, MdOutlineOndemandVideo, MdExpandMore, MdOutlineExpandMore } from "react-icons/md";
+import { AiOutlineShop } from "react-icons/ai";
+import { BsStopwatch } from "react-icons/bs";
+
 const Sidebar = () => {
   return (
     <div className="hidden lg:inline-flex flex-col py-2 pl-2 max-w-xl lg:min-w-[302px">
@@ -14,11 +19,13 @@ const Sidebar = () => {
         />
         <p className="hidden sm:inline-flex font-medium"> Gilbert Ngeno</p>
       </div>
-
-      <div className="flex items-center space-x-2 py-3 pl-4 hover:bg-gray-200 rounded-l-xl cursor-pointer">
-        <ImUsers className="h-8 w-8 text-blue-500" />
-        <p className="hidden sm:inline-flex font-medium">Users</p>
-      </div>
+      <SidebarItem Icon={ImUsers} value="Friends" />
+      <SidebarItem Icon={MdGroups} value="Groups" />
+      <SidebarItem Icon={AiOutlineShop} value="Markeplace" />
+      <SidebarItem Icon={MdOutlineOndemandVideo} value="Watch" />
+      <SidebarItem Icon={BsStopwatch} value="On this day"/>
+      <SidebarItem Icon={MdOutlineExpandMore} value="See More" />
+      
     </div>
   );
 };
